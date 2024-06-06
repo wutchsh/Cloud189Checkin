e97d9c15b7d314af5da0fc56e381584ba5193b85
wutchsh <48751359+wutchsh@users.noreply.github.com>
2024-06-07 00:42:42 +0800

    更新 app.js
    变更通知内容标题

--- src/app.js
+++ src/app.js
@@ -531,7 +531,8 @@ async function main() {
   } finally {
     const events = recording.replay();
     const content = events.map((e) => `${e.data.join("")}`).join("  \n");
-    push("天翼云盘自动签到任务", content);
+//    push("天翼云盘自动签到任务", content);
+      push(content,"0");
     recording.erase();
   }
 })();
